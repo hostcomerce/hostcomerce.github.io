7bb570b9e85fd2eed865115e4ab8a899540aad32
juan cruz lamanna <82359215+hostcomerce@users.noreply.github.com>
2026-04-20 08:09:16 -0300

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,7 @@
 /** --- VARIABLES Y ESTADO --- **/
 let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
 // URL de tu ÚLTIMA implementación (asegurate de que termine en /exec)
-const urlAPI = "https://script.google.com/macros/s/AKfycbwBOv6Wpf6Zj9PXxCsjEvecca_oIiYn9dQ8EQkWDZ8r1kKZ_Y3tQo2KlBbvPs0Cb-h2CQ/exec";
+const urlAPI = "https://script.google.com/macros/s/AKfycbzlG3uawZthB-sAXMoMhj7AgElIlmdTHBPDIMOB-Lu5V2_r4YsnWWnESovTLO8u3i4Kkw/exec";
 
 // Referencias al DOM
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
